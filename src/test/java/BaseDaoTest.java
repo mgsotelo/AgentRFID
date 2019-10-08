7ac107a882d75e7b616d2ed391aa6f1c68d7c68d
Mario Sotelo <mgsotelo@pucp.pe>
2019-10-07 19:13:21 -0500

    adding first test

---
+++ src/test/java/BaseDaoTest.java
@@ -0,0 +1,18 @@
+import com.google.cloud.firestore.Firestore;
+import daos.BaseDao;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BaseDaoTest {
+
+    @Test
+    public void getFirestoreDB() {
+        try{
+            Firestore fdb = BaseDao.getFirestoreDB();
+            assertEquals("alumnos", fdb.collection("alumnos").getPath());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
