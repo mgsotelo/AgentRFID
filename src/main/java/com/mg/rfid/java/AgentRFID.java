bd4fd0cfd8a576965a5d11262e5b399d4f0fc1de
Mario Sotelo <mgsotelo@pucp.pe>
2019-10-28 18:02:31 -0500

    Pre-release update
    Adding support for Technician
    Adding hostname discovery of Impinj reader
    Adding comments

--- src/main/java/com/mg/rfid/java/AgentRFID.java
+++ src/main/java/com/mg/rfid/java/AgentRFID.java
@@ -2,19 +2,19 @@ package com.mg.rfid.java;
 
 import com.impinj.octane.ImpinjReader;
 import com.mg.rfid.java.beans.Equipment;
+import com.mg.rfid.java.beans.Technician;
 import com.mg.rfid.java.beans.Temporal;
 import com.google.cloud.firestore.Firestore;
-import com.mg.rfid.java.daos.BaseDao;
-import com.mg.rfid.java.daos.Dao;
-import com.mg.rfid.java.daos.EquipmentDao;
-import com.mg.rfid.java.daos.TemporalDao;
+import com.mg.rfid.java.daos.*;
 import com.mg.rfid.java.rfid.UtilRFID;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Optional;
 
 public class AgentRFID {
 
-    private static Dao eqdao, tempdao;
+    private static Dao eqdao, tempdao, techdao;
 
 
     public static void main(String[] args)  {
@@ -24,21 +24,50 @@ public class AgentRFID {
         try {
             eqdao = new EquipmentDao();
             tempdao = new TemporalDao();
+            techdao = new TechnicianDao();
+
             Firestore fdb = BaseDao.getFirestoreDB();
             UtilRFID myrfid = new UtilRFID();
-            myrfid.setHostname("192.168.35.239");
+            String s, hostname="";
+            Process p;
+
+            try {
+
+
+                p = Runtime.getRuntime().exec("sudo arp-scan 192.168.35.0/24 | grep -i impinj | cut -d ' ' -f1 | head -1");
+                BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                while( ((s = br.readLine())!= null)){
+                    if(s.contains("192")){
+                        System.out.println("[AgentRFID] Hostname detectado. El hostname es "+ hostname);
+                        hostname = s;
+                    }else {
+                        System.out.println("[AgentRFID] Hostname no ha podido ser detectado.");
+                        hostname = "192.168.35.239";
+                        System.out.println("[AgentRFID] Hostname por default es "+ hostname);
+                    }
+                }
+                p.waitFor();
+                System.out.println("[AgentRFID] Finalizando ejecución de comandos en vm linux");
+                p.destroy();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            myrfid.setHostname(hostname);
             myrfid.setReader(new ImpinjReader());
             //myrfid.setHostname("SpeedwayIoT");
 
             AgentRFID agent = new AgentRFID();
-            agent.run(eqdao, tempdao, fdb, myrfid);
+            agent.run(eqdao, tempdao, techdao, fdb, myrfid);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    private synchronized void run(Dao eqdao, Dao tempdao, Firestore fdb, UtilRFID myrfid) {
+    private synchronized void run(Dao eqdao, Dao tempdao, Dao techdao, Firestore fdb, UtilRFID myrfid) {
 
         while(true){
             try {
@@ -51,9 +80,11 @@ public class AgentRFID {
                 if(!epc.equals("")){
                     Optional<Equipment> currenteq= eqdao.get(epc, fdb);
 
-                    if(currenteq.isPresent()){
+                    Optional<Technician> currentTech = techdao.get(epc,fdb);
 
-                        Optional<Temporal> temp = tempdao.get(epc,fdb);
+                    Optional<Temporal> temp = tempdao.get(epc,fdb);
+
+                    if(currenteq.isPresent()){
 
                         if(temp.isPresent()){
 
@@ -61,12 +92,15 @@ public class AgentRFID {
 
                         } else{
 
-                            Temporal myoldeq = new Temporal();
-                            myoldeq.setEpc(epc);
+                            Temporal myoldeq = temp.get();
                             tempdao.insert(myoldeq,fdb);
 
-
                         }
+
+                    } else if (currentTech.isPresent()){
+
+                        Technician mytech = currentTech.get();
+                        tempdao.insert(mytech,fdb);
 
                     } else {
 
